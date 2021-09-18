000a3175c79336923427dad1261cce492efe8939
MidD3Vil <79802337+MidD3Vil@users.noreply.github.com>
2021-09-18 12:06:29 -0300

    Update decode.py

--- database/decode.py
+++ database/decode.py
@@ -1,24 +1,21 @@
-from bank import bank
-
-bank = bank()
-
-word = str(input('\nDigite o texto para decrypt: '))
-
-len = len(word)
-list = []
-count = 0
-
-for c in range(len):
-    if count % 2 == 0:
-        list.append(word[count])
-    else:
-        pass
-    count += 1
-print('\n\n')
-
-count = 0
-for element in list:
-    place = bank.index(element)
-    place_plus = (place - count)
-    print(f'{bank[place_plus]}', end='')
-    count += 1
+def encode(word, key):
+    len_str = len(word)
+    from Bank import bank
+    end_list = []
+    # pad = ['m', 'i', 'd']
+    count = count_add = count_key = 0
+    for c in range(len_str):
+        place = bank.index(word[count])
+        place_plus = (place + int(key[count_key]))
+        end_list.append(f'{bank[place_plus]}{count_add}')
+        count += 1
+        count_add += 1
+        count_key += 1
+        if count_add == 9:
+            count_add = 0
+        if count_key == 4:
+            count_key = 0
+    end_list = ''.join(end_list)
+    str_list = str(end_list)
+    result = ''.join(map(str, str_list))
+    return result

71e5581df84ae5a47a91f5202cbc0070dcade35e
MidD3Vil <79802337+MidD3Vil@users.noreply.github.com>
2021-09-18 12:07:08 -0300

    Update encode.py

--- database/encode.py
+++ database/encode.py
@@ -1,20 +1,21 @@
-from bank import bank
-
-bank = bank()
-
-word = str(input('\nDigite o texto para encrypt: '))
-
-len = len(word)
-# pad = ['m', 'i', 'd']
-count = 0
-count2 = 0
-
-for c in range(len):
-    place = bank.index(word[count])
-    place_plus = (place + count)
-    print(f'{bank[place_plus]}{count2}', end='')
-    count += 1
-    count2 += 1
-    if count2 == 9:
-        count2 = 0
-print('\n\n')
+def encode(word, key):
+    len_str = len(word)
+    from Bank import bank
+    end_list = []
+    # pad = ['m', 'i', 'd']
+    count = count_add = count_key = 0
+    for c in range(len_str):
+        place = bank.index(word[count])
+        place_plus = (place + int(key[count_key]))
+        end_list.append(f'{bank[place_plus]}{count_add}')
+        count += 1
+        count_add += 1
+        count_key += 1
+        if count_add == 9:
+            count_add = 0
+        if count_key == 4:
+            count_key = 0
+    end_list = ''.join(end_list)
+    str_list = str(end_list)
+    result = ''.join(map(str, str_list))
+    return result

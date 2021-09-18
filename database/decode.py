6b8d7a409da4f1c9fb772fc8a4349d6c1d7b2617
MidD3Vil <79802337+MidD3Vil@users.noreply.github.com>
2021-09-18 12:20:09 -0300

    Create decode.py

--- database/decode.py
+++ database/decode.py
@@ -46,5 +46,5 @@ def decode():
     end_list = ''.join(end_list)
     str_list = str(end_list)
     result = ''.join(map(str, str_list))
-    print('Texto Criptografado com sucesso!') 
+    print('Texto Descriptografado com sucesso!') 
     return result

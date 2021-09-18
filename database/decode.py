db234c8a742a0c3c52ddc97b1bf101ac8cfced1f
MidD3Vil <79802337+MidD3Vil@users.noreply.github.com>
2021-09-18 12:18:16 -0300

    Update decode.py

--- database/decode.py
+++ database/decode.py
@@ -1,4 +1,27 @@
-def decode(word, key):
+def decode():
+    import os
+
+    ## Tabela de cores ANSI (Python) ##
+    Mblack = '\033[1;30m'   # Preto
+    Ired = '\033[1;31m'     # Vermelho
+    Dgreen = '\033[1;32m'   # Verde
+    Nyellow = '\033[1;33m'  # Amarelo
+    Iblue = '\033[1;34m'    # Azul
+    Gpurple = '\033[1;35m'  # Roxo
+    Hcyan = '\033[1;36m'    # Ciano
+    Twhite = '\033[1;37m'   # Branco
+    VRCRM = '\033[0;0m'     # Remover
+    def clear():
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    clear()
+        
+    print(f'Script de Ofuscação by Dr Midnight')
+    
+    print(f'{Gpurple}====================================\n')
+    word = str(input(f'{Gpurple}Digite o texto para encrypt:{VRCRM} '))
+    key = str(input(f'{Gpurple}Digite o seu pin de 4 digitos:{VRCRM} '))
+    print(f'\n{Gpurple}====================================')
     len_str = len(word)
     from Bank import bank
     # pad = ['m', 'i', 'd']
@@ -23,4 +46,4 @@ def decode(word, key):
     end_list = ''.join(end_list)
     str_list = str(end_list)
     result = ''.join(map(str, str_list))
-    return result
+    print('Texto Criptografado com sucesso!\n >>>' return result)
